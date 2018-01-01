cb7325a779baeb86484bce79626f0e706e3603f3
Håkan Kindström Arnoldson <hakan@arnoldson.net>
2018-01-01 19:01:02 +0300

    setup

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware
+} from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+
+});
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+);
+
+export default store;
